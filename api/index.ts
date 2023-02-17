891eae813c70f0216f22bd3dc54f1ca7d86413f7
Migue Godoy <mgodoysoro1988@gmail.com>
2023-02-17 16:13:35 +0100

    add wrangler and themes endpoint

---
+++ api/index.ts
@@ -0,0 +1,36 @@
+import { ExecutionContext } from "hono/dist/types/types"
+import { Hono } from "hono"
+import themes from "../db/themes.json"
+
+export interface Env {
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+  //
+  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+  // MY_SERVICE: Fetcher;
+}
+
+const app = new Hono()
+
+app.get("/", (ctx) => {
+  return ctx.json({
+    endpoints: [
+      {
+        url: "/themes",
+        description: "Get al oh-my-zsh themes",
+      },
+    ],
+  })
+})
+
+app.get("/themes", (ctx) => {
+  return ctx.json(themes)
+})
+
+export default app
