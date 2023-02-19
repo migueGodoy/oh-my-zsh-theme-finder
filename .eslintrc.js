496a34c13d10f7f5b125246b28d8fe43a809f822
Migue Godoy <mgodoysoro1988@gmail.com>
2023-02-19 23:07:50 +0100

    Add astro with tailwind

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['standard', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:astro/recommended'
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
@@ -9,5 +14,17 @@ module.exports = {
     'space-before-function-paren': 'off',
     'comma-dangle': 'error',
     'eol-last': 'error'
-  }
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      },
+      rules: {
+        'astro/no-set-html-directive': 'error'
+      }
+    }
+  ]
 }
